d2a29afabd12bf2f35e01c082f3ec5efe8e6d8c2
Георгий Коротков <gakorotkov@edu.hse.ru>
2020-09-29 23:51:43 +0300

    complete

--- 01 Module/03Seminar/03Seminar/Program.cs
+++ 01 Module/03Seminar/03Seminar/Program.cs
@@ -1,12 +1,157 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Library;
 
 namespace _03Seminar
 {
     class Program
     {
+        private static Journal journal { get; set; }
+
+        private static StreamReader data { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                Console.Clear();
+
+                journal = new Journal();
+
+                if (!IsDataCorrect())
+                {
+                    Console.WriteLine("Проблема с файлом или некоректные данные");
+                    goto End;
+                }
+
+                journal.MySerialize("out.ser");
+                var spisok = Journal.MyDeserialize("out.ser");
+
+                foreach (var pair in spisok)
+                {
+                    Console.WriteLine(pair.ToString());
+                }
+
+                Console.WriteLine("\n");
+
+                foreach (var pair in spisok.GetWorst(new Random().Next(0, 6)))
+                {
+                    Console.WriteLine(pair.ToString());
+                }
+
+                End:
+                data.Dispose();
+                Console.WriteLine("\npress escpae to exit or any key to run again");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        ///// <summary>
+        ///// Добавление в results данных из файла
+        ///// </summary>
+        ///// <returns></returns>
+        //private static bool GetResults()
+        //{
+        //    try
+        //    {
+        //        data = new StreamReader("data.txt");
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e);
+        //        return false;
+        //    }
+
+        //    while (!data.EndOfStream)
+        //    {
+        //        string[] dataStrings = data.ReadLine().Split(' ');
+
+        //        double average = 0;
+
+        //        for (int i = 1; i < dataStrings.Length; i++)
+        //        {
+        //            average += int.Parse(dataStrings[i]);
+        //        }
+
+        //        average /= dataStrings.Length - 1;
+
+        //        journal += new Pair<string, double>() {item2 = average, item1 = dataStrings[0]};
+        //    }
+
+        //    return true;
+        //}
+
+        /// <summary>
+        /// Проверка файла на корректность и запись в Journal если данные верны.
+        /// </summary>
+        /// <returns></returns>
+        private static bool IsDataCorrect()
+        {
+            try
+            {
+                data = new StreamReader("data.txt", Encoding.UTF8);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            while (!data.EndOfStream)
+            {
+                string[] dataString = data.ReadLine().Split(' ');
+
+                for (int i = 0; i < dataString[0].Length; i++)
+                {
+                    if (!(dataString[0][i] >= 'а' && dataString[0][i] <= 'я' ||
+                        dataString[0][i] >= 'А' && dataString[0][i] <= 'Я'))
+                    {
+                        return false;
+                    }
+                }
+
+                for (int i = 1; i < dataString.Length; i++)
+                {
+                    if (!int.TryParse(dataString[i], out int tmpResult))
+                    {
+                        return false;
+                    }
+
+                    if (tmpResult < 0 || tmpResult > 10)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            data.Close();
+
+            try
+            {
+                data = new StreamReader("data.txt");
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            while (!data.EndOfStream)
+            {
+                string[] dataStrings = data.ReadLine().Split(' ');
+
+                double average = 0;
+
+                for (int i = 1; i < dataStrings.Length; i++)
+                {
+                    average += int.Parse(dataStrings[i]);
+                }
+
+                average /= dataStrings.Length - 1;
+
+                journal += new Pair<string, double>() { item2 = average, item1 = dataStrings[0] };
+            }
+
+            return true;
         }
     }
 }
---
+++ 01 Module/03Seminar/Library/Class1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Library
+{
+    public class Pair<T, U> : IComparable<Pair<T, U>>
+        where U : IComparable<U>
+    {
+        public T item1;
+        public U item2;
+
+        public int CompareTo(Pair<T, U> pair)
+        {
+            return this.item2.CompareTo(pair.item2);
+        }
+
+        public override string ToString()
+        {
+            return item1.ToString() + " " + item2.ToString();
+        }
+    }
+
+    public class Journal
+    {
+        /// <summary>
+        /// Список студентов и их оценок
+        /// </summary>
+        public List<Pair<string, double>> results;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public Journal()
+        {
+            results = new List<Pair<string, double>>();
+        }
+
+        public override string ToString()
+        {
+            return results[0].item1.ToString() + " " + results[0].item2.ToString();
+        }
+
+        public static Journal operator +(Journal journal, Pair<string, double> pair)
+        {
+            journal.results.Add(pair);
+            return new Journal() {results = journal.results};
+        }
+
+        /// <summary>
+        /// Перечисление results в порядке невозрастания
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator()
+        {
+            List<Pair<string, double>> sortedResults = results;
+            sortedResults.Sort();
+            for (int i = sortedResults.Count-1; i >= 0; i--)
+            {
+                yield return sortedResults[i];
+            }
+        }
+
+        /// <summary>
+        /// Перечисление худших студентов по оценкам
+        /// </summary>
+        /// <param name="N">Количество студентов для вывода</param>
+        /// <returns></returns>
+        public IEnumerable GetWorst(int N)
+        {
+            List<Pair<string, double>> sortedResults = results;
+            sortedResults.Sort();
+            for (int i = 0; i < (N <= sortedResults.Count ? N : sortedResults.Count); i++)
+            {
+                yield return sortedResults[i];
+            }
+        }
+
+        /// <summary>
+        /// Сериализует текущий объект класса Journal в xml
+        /// </summary>
+        /// <param name="path">путь до файла</param>
+        public void MySerialize(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                XmlSerializer serializer = new XmlSerializer(this.GetType());
+
+                serializer.Serialize(fs,this);
+            }
+        }
+
+        /// <summary>
+        /// Десериализует xml файл в объект класса Journal
+        /// </summary>
+        /// <param name="path">путь до файла</param>
+        /// <returns></returns>
+        public static Journal MyDeserialize(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Journal));
+
+                return serializer.Deserialize(fs) as Journal;
+            }
+        }
+    }
+}
